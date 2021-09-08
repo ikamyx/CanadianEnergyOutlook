2a623acf2ffaa6783f885d2c519561a257dee55e
ikamyx <ikammmy@gmail.com>
2021-09-07 21:51:42 -0400

    commit

--- js/modules/scaleY_bar.js
+++ js/modules/scaleY_bar.js
@@ -11,8 +11,8 @@ function scaleY_bar(data, yAxisHeight) {
         newLastTick = lastTick + (ticks[1] - ticks[0]),
         newFirstTick = firstTick - (ticks[1] - ticks[0]);
 
-    console.log(scaleY.domain()[0], scaleY.domain()[1])
-    console.log(scaleY.ticks())
+    // console.log(scaleY.domain()[0], scaleY.domain()[1])
+    // console.log(scaleY.ticks())
 
     if (lastTick<scaleY.domain()[1]) {
             ticks.push(newLastTick);
