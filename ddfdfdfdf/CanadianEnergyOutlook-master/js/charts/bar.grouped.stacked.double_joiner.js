e1bb66408f38dcab08f6883effe9af3fe6898607
ikamyx <ikammmy@gmail.com>
2021-09-17 17:56:21 -0400

    commit

--- ddfdfdfdf/CanadianEnergyOutlook-master/js/charts/bar.grouped.stacked.double_joiner.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function bar_grouped_stacked_double_joiner(content, colors, settings, language) {
-    content.forEach((element, i) => {
-        bar_grouped_stacked_double(element.data, element.metadata, colors, settings, language,  i+1)
-    });
-}
--- ddfdfdfdf/CanadianEnergyOutlook-master/js/modules/distributionCalculation_bar_stacked.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function distributionCalculation_bar_stacked(distribution, xAxisWidth, setting) {
-    distribution.barSpace = distribution.barSpaceNumber ? (xAxisWidth * (setting.distribution.barSpacePercent/100)) / distribution.barSpaceNumber : 0;
-    distribution.barWidth = (xAxisWidth * (setting.distribution.barWidthPercent/100)) / distribution.barNumber;
-}
-    
